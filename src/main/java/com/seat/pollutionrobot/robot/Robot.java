43c3bf5d8035c3781493f93f1c6169293566c1ee
Jordi Gramunt Jorda <jgramunt@everis.com>
2020-03-30 14:41:54 +0200

    Set Robot template

---
+++ src/main/java/com/seat/pollutionrobot/robot/Robot.java
@@ -0,0 +1,27 @@
+package com.seat.pollutionrobot.robot;
+
+public class Robot {
+
+    private String polyline;
+    private String actualPosition;
+    private String speed;
+    private boolean isRobotOn;
+
+
+    public void start() {
+
+    }
+
+    public void stop() {
+
+    }
+
+    public String report() {
+        return null;
+    }
+
+    public void setPolyline(String polyline) {
+        this.polyline = polyline;
+    }
+
+}
