bf67de51d2c2fb5f1fca7cededfe93f5b196dd56
Jordi Gramunt Jorda <jgramunt@everis.com>
2020-03-30 13:11:23 +0200

    First commit

---
+++ src/test/java/com/seat/pollutionrobot/PollutionRobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seat.pollutionrobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollutionRobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
